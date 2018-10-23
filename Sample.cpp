4a0d48883c1dc5a8a93f3d411f7b2cdbe7d4b847
macabdul9 <abdulwaheed1513@gmail.com>
2018-10-23 15:43:31 +0530

    string subsequence

--- Sample.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-#define endl "\n"
-using namespace std;
-int main(){
-  string s = 'a' + 'b';
-  cout << s << endl;
-}
--- subsequence.cpp
+++ subsequence.cpp
@@ -3,49 +3,23 @@
 using namespace std;
 
 /*
-Brute-force approach solution which practicallt runs faster
+  recursive solution
+  but for larger input upper this will be slow
 */
-void subseq1(string s){
-
-
-  if(s.length() == 1){
-    cout << s << endl;
-    return;
-  }
-  string ans = "";
-  for (int i = s.length(); i > 0; i--){
-    ans = s.substr(0, i);
-    cout << ans  << endl;
-  }
-
-  for(int i = 2; i < s.length(); i++){
-    ans = "";
-    ans.push_back(s[0]);ans.push_back(s[i]);
-    cout << ans << endl;
-  }
-  subseq1(s.substr(1, s.length()));
-}
-
-
-/*
-  optimized solution
-  but for larger input upper one runs quicker than this
-*/
-void subseq2(string exp, string ans, int n){
+void subseq(string exp, string ans, int n){
   if (n == exp.length()) {
     /* code */
     cout<< ans << endl;
     return;
   }
-  subseq2(exp, ans, n + 1);
+  subseq(exp, ans, n + 1);
   ans.push_back(exp[n]);
-  subseq2(exp, ans, n + 1);
+  subseq(exp, ans, n + 1);
 }
 int main(int argc, char const *argv[]) {
   /* code */
   string e, a;
   cin>>e;
-  // subseq1(e);
-  subseq2(e,a,0);
+  subseq(e,a,0);
   return 0;
 }
