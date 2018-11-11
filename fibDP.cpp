4387a5966d066863b3ff0c053bba23b51af412d6
macabdul9 <abdulwaheed1513@gmail.com>
2018-11-12 02:17:16 +0530

    fibnacci number - dynamic programming

--- fibDP.cpp
+++ fibDP.cpp
@@ -1,31 +1,27 @@
 #include <bits/stdc++.h>
 #define endl "\n"
 #define ll long long
-#define MAX 100000001
+#define MAX 1000001
 using namespace std;
 
-ll dp[MAX];
-
-ll getFib(ll n){
+ll getFib(ll n, ll *dp){
   if(n < 2){
-    if(dp[n])
-      return dp[n];
-    else{
-      dp[n] = n;
-      return dp[n];
-    }
+    dp[n] = n;
+    return dp[n];
   }
-  if(dp[n])
+  if(dp[n] != -1)
     return dp[n];
   else
-    dp[n] = getFib(n - 1) + getFib( n - 2);
+    dp[n] = getFib(n - 1, dp) + getFib( n - 2, dp);
   return dp[n];
 }
 int main(int argc, char const *argv[]) {
   ll n;
+  ll dp[MAX];
+  memset(dp, -1, sizeof(dp));
   while(true){
     cin >>n;
-    cout << getFib(n);
+    cout << getFib(n, dp);
   }
   return 0;
 }
