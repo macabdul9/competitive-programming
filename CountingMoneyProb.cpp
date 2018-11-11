7b7a0ed87f673a78bff5d4c9e3617ed9200c865d
macabdul9 <abdulwaheed1513@gmail.com>
2018-11-12 01:48:24 +0530

    counting money problem - greedy approach

--- CountingMoneyProb.cpp
+++ CountingMoneyProb.cpp
@@ -5,30 +5,35 @@
 using namespace std;
 
 const ll notes[] {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
-int current = sizeof(notes)/sizeof(*notes) - 1;
-
-void getNotes(ll amount, vector<ll> &result){
+/* this is greedy technique solution that might not work for every currency
+  const ll notes[] {1, 2, 5, 7, 10 20, 50, 100, 200, 500, 2000};
+  in the above notes suppose we want 14 then below solution will give 10, 2, 2
+  counting total = 3 Notes - 10 2 2
+  while optimal solution should have  total = 2 notes - 7 7
+*/
+void getNotes(ll amount, vector<ll> &result, int start){
   if(!amount)
     return;
-  for(int i = current ; i > -1; i--){
+  for(int i = start ; i > -1; i--){
     if(amount == notes[i] or amount > notes[i]){
       result.push_back(notes[i]);
       amount -= notes[i];
-      current = i;
+      start = i;
       break;
     }
   }
-  getNotes(amount, result);
+  getNotes(amount, result, start);
 }
 
 int main(int argc, char const *argv[]) {
   /* code */
   ll amount;
   vector<ll> v;
+  int current = sizeof(notes)/sizeof(*notes) - 1;
   while (true) {
     /* code */
     cin >> amount;
-    getNotes(amount, v);
+    getNotes(amount, v,  current);
     for(auto& e:v)
       cout << e << " ";
     cout << "\ntotal notes = " << v.size() <<endl;
