0cd57762854991fcf4c594eed2f71e740689d97e
macabdul9 <abdulwaheed1513@gmail.com>
2019-02-21 19:09:24 +0530

    via terminal

---
+++ randchar.cpp
@@ -0,0 +1,48 @@
+/*
+ * @author    : macab (macab@debian)
+ * @file      : randchar
+ * @created   : Thursday Feb 21, 2019 18:48:33 IST
+*/
+#include<bits/stdc++.h>
+#define endl 		         "\n"
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define clr(x)                x.clear()
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define psi                   pair<string, int>
+#define pci                   pair<char, int>
+#define all(p)                p.begin(),p.end()
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define vi                    vector<int>
+#define vs                    vector<string>
+#define si                    set<int>
+#define ss                    set<string>
+#define mii                   map<int, int>
+#define msi                   map<string, int>
+#define umii                  unordered_map<int, int>
+#define umsi                  unordered_map<string, int>
+typedef long long int ll;
+typedef unsigned int uint;
+using namespace std;
+
+int main(){
+    /*code goes here*/
+    srand(time(0));
+    char mychar;
+    while(true){
+        // generate random char after one second
+        mychar = char(97 + rand()% (122 - 96));
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+        cout << mychar << endl;
+
+    }
+    return 0;
+}
+
