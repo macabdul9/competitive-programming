6372446be6d9415c5edf7dcc32493d285163a0cd
macabdul9 <abdulwaheed1513@gmail.com>
2019-03-09 12:17:38 +0530

    via terminal

--- ChefgotRecipes.cpp
+++ ChefgotRecipes.cpp
@@ -5,7 +5,7 @@
 */
 #include<bits/stdc++.h>
 #define endl 		         "\n"
-#define merge(a, b)           a##b 
+#define merge(a, b)           a##b
 #define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
 #define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
 #define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
@@ -47,14 +47,62 @@ string removeduplicate(string s){
     return ans;
 }
 
+ull countgoodmeal(vector<string> &dishes){
+    if(dishes.size() < 2) return 0;
+
+    ull count = 0;
+    string vowels = "aeiou" ;
+    vector<string> compiled_dishes;
+    string not_present;
+
+    loop(i, 0, dishes.size()){
+
+
+        for(int k = 0; k < vowels.length(); k++){
+            if(dishes[i].find(vowels[k]) == string::npos){
+                not_present.push_back(vowels[k]);
+            }
+        }
+
+        loopr(j, compiled_dishes.size() - 1 , 0){
+                bool b = true;
+                for(int l = 0; l < not_present.length(); l++){
+                    if(compiled_dishes[j].find(not_present[l]) == string::npos){
+                            b = false;
+                            break;
+                    }
+                }
+                if(b){
+                    count++;
+                }
+
+        }
+        compiled_dishes.push_back(dishes[i]);
+        not_present.clear();
+
+    }
+
+    compiled_dishes.clear();
+    return count;
+
+}
+
 int main(){
     ios::sync_with_stdio(0);
-    cin.tie(NULL);
 
-    /*code goes here*/
-    string a = "aaooaoaooa";
-    string b = "aeioooeeiiaiei";
-    cout << removeduplicate(a + b) << endl;
+    int t,n;
+    string str;
+    vector<string> dishes;
+    cin >> t;
+    while(t-- > 0){
+        cin >> n;
+        loop(i, 0, n){
+            cin >> str;
+            dishes.push_back(removeduplicate(str));
+        }
+        cout << countgoodmeal(dishes) << endl;
+        dishes.clear();
+    }
 
     return 0;
 }
