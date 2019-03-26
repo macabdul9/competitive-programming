d0573ddaaea41c5654488acad9c5d5f16ae8cff8
macabdul9 <abdulwaheed1513@gmail.com>
2019-03-26 15:45:20 +0530

    via terminal

---
+++ templatefunction.cpp
@@ -0,0 +1,53 @@
+/*
+ * @author    : macab (macab@debian)
+ * @file      : templatefunction
+ * @created   : Tuesday Mar 26, 2019 15:26:31 IST
+*/
+#include<bits/stdc++.h>
+#define endl                  "\n"
+#define merge(a, b)           a##b
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define psi                   pair<string, int>
+#define pci                   pair<char, int>
+#define all(p)                p.begin(),p.end()
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define vi                    vector<int>
+#define vll                   vector<long long int>
+#define vs                    vector<string>
+#define si                    set<int>
+#define ss                    set<string>
+#define sll                   set<long long int>
+#define mii                   map<int, int>
+#define mll                   map<long long int, long long int>
+#define msi                   map<string, int>
+#define umii                  unordered_map<int, int>
+#define umsi                  unordered_map<string, int>
+typedef long long int ll;
+typedef unsigned int uint;
+typedef unsigned long long int ull;
+using namespace std;
+
+template<class T>
+T tfun(T &a, T &b){
+        return a + b;
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+
+	int a = 0, b = 23;
+    float x = 21.4, y = 3.4;
+    cout << tfun(a, b) << " " << tfun(x, y) << endl;
+
+	return 0;
+}
+
