ef3262f2b7a0722e30a8f911d3b2583d33db3413
macabdul9 <abdulwaheed1513@gmail.com>
2019-03-31 23:02:53 +0530

    via terminal

---
+++ segmentedSiev.cpp
@@ -0,0 +1,63 @@
+/*
+ * @author    : macab (macab@debian)
+ * @file      : segmentedSiev
+ * @created   : Sunday Mar 31, 2019 02:12:14 IST
+*/
+#include<bits/stdc++.h>
+#define endl                  "\n"
+#define merge(a, b)           a##b
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define psi                   pair<string, int>
+#define pci                   pair<char, int>
+#define all(p)                p.begin(),p.end()
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define vi                    vector<int>
+#define vll                   vector<long long int>
+#define vs                    vector<string>
+#define si                    set<int>
+#define ss                    set<string>
+#define sll                   set<long long int>
+#define mii                   map<int, int>
+#define mll                   map<long long int, long long int>
+#define msi                   map<string, int>
+#define umii                  unordered_map<int, int>
+#define umsi                  unordered_map<string, int>
+typedef long long int ll;
+typedef unsigned int uint;
+typedef unsigned long long int ull;
+using namespace std;
+
+// this fun returns primes upto r ---- the maximum value of r could be 1000000000001
+#define m 100000
+vector<bool> sieve(){
+    vector<bool> isPrime(m, true);
+    isPrime[0] = isPrime[1] = false;
+
+    rloop(p, 2, sqrt(m)){
+        if(isPrime[p]){
+            for(int i = p*p; i <= m; i += p)
+                    isPrime[i] = false;
+        }
+    }
+
+    return isPrime;
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+
+
+
+
+	return 0;
+}
+
