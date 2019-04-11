6333463296a18f0159ae0b992c322a02d337d498
macabdul9 <abdulwaheed1513@gmail.com>
2019-04-11 21:56:04 +0530

    via terminal

---
+++ virtualfunction.cpp
@@ -0,0 +1,63 @@
+/*
+ * @author    : macab (macab@debian)
+ * @file      : virtualfunction
+ * @created   : Thursday Apr 11, 2019 14:47:18 IST
+*/
+#include<bits/stdc++.h>
+#define endl                  "\n"
+#define merge(a, b)           a##b
+#define loop(i,a,b)           for(int i=(int)a;i<(int)b;++i)
+#define rloop(i,a,b)          for(int i=(int)a;i<=(int)b;++i)
+#define loopl(i,a,b)          for(ll i=(ll)a;i<(ll)b;++i)
+#define loopr(i,a,b)          for(int i=(int)a;i>=(int)b;--i)
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define psi                   pair<string, int>
+#define pci                   pair<char, int>
+#define all(p)                p.begin(),p.end()
+#define max(x,y)              (x>y)?x:y
+#define min(x,y)              (x<y)?x:y
+#define vi                    vector<int>
+#define vll                   vector<long long int>
+#define vs                    vector<string>
+#define si                    set<int>
+#define ss                    set<string>
+#define sll                   set<long long int>
+#define mii                   map<int, int>
+#define mll                   map<long long int, long long int>
+#define msi                   map<string, int>
+#define umii                  unordered_map<int, int>
+#define umsi                  unordered_map<string, int>
+typedef long long int ll;
+typedef unsigned int uint;
+typedef unsigned long long int ull;
+using namespace std;
+
+class base{
+    public:
+            virtual void show(){
+                cout << " im show() of base class" << endl;
+            }
+};
+
+class derived:public base{
+    public:
+            void show(){
+                cout << " im show() of derived class" << endl;
+            }
+};
+
+int main(){
+	ios::sync_with_stdio(0);
+    derived d;
+    base* b;
+    b = &d;
+    b->show();
+
+
+	return 0;
+}
+
